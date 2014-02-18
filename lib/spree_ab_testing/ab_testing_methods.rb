1ab542cb053069037f8412c5eb639712db39d5c6
abhishek <abhishek.jain@vinsol.com>
2014-02-19 01:00:34 +0530

    abtest method module

---
+++ lib/spree_ab_testing/ab_testing_methods.rb
@@ -0,0 +1,16 @@
+module AbTesting
+  module AbTestingMethod
+    def self.included(klass)
+      klass.class_eval do
+        before_filter :fetch_experiments, :unless => lambda {|c| c.restricted_to_experiment? }
+      end
+    end
+    def fetch_experiments
+      @experiments_with_variation = Experiment.with_variations.where(:relative_path => request.path)
+    end
+
+    def restricted_to_experiment?
+      controller_name == "experiments" || controller_name == "variations" || params[:no_show].present?
+    end
+  end
+end
